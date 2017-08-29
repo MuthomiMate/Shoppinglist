93fa38a2cc60bb3c847dcc661171a948db0ef099
Muthomi Mate <muthomimate@gmail.com>
2017-08-29 17:56:54 +0300

    [Update]test_user.py

--- test_user.py
+++ test_user.py
@@ -6,15 +6,20 @@ class UsersTest(unittest.TestCase):
 	def setUp(self):
 		self.mimi=User()
 
+
+"""tests if the email is correct"""
 	def email_test(self):
 		self.assertEqual(self.mimi.email, 'muthomimate@gmail.com', msg='Invalid email')
 
+"""tests if the pname is correct"""
 	def name_test(self):
 		self.assertEqual(self.mimi.name, 'muthomi', msg='Invalid name')
-	
+
+"""tests if the password is correct"""	
 	def password_test(self):
 		self.assertEqual(self.mimi.password, 'muthomi', msg='Invalid Password')
 
+"""tests if the username is correct"""	
 	def username_test(self):
 		self.assertEqual(self.mimi.username, 'muthomi', msg='Invalid Username')
 		
--- user.py
+++ user.py
@@ -1,6 +1,6 @@
 class User(object):
 	"""docstring for user"""
-	def __init__(self, email='muthomimat@gmail.com', password='muthomi', username='muthomi', name='muthomi'):
+	def __init__(self, email='muthomimate@gmail.com', password='muthomi', username='muthomi', name='muthomi'):
 		self.email=email
 		self.password=password
 		self.username=username
