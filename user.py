2ab1d3e6fe3a7bb52cd31e759beb0c5e8b2670c1
Muthomi Mate <muthomimate@gmail.com>
2017-08-29 22:01:24 +0300

    [New]users.py added register function

--- user.py
+++ user.py
@@ -1,11 +1,43 @@
-class User(object):
-	"""docstring for user"""
-	def __init__(self, email='muthomimate@gmail.com', password='muthomi', username='muthomi', name='muthomi'):
-		self.email=email
-		self.password=password
-		self.username=username
-		self.name=name
+import re
 
-	
-		
-		
+users = {};
+
+
+class User(object):
+    """
+    Class to handle  user functions
+    """
+
+    def __init__(self, name=None, username=None, email=None, password=None):
+        """ Initializing  class instance variables"""
+        self.name = name
+        self.email = email
+        self.password = password
+
+        def register(self, email, name, password, cpassword):
+            """defining method to create account"""
+            if name != '' and username and email != '' and password != '':
+                if username not in users.keys():
+                    if email not in users.keys():
+                        if password == cpassword:
+                            regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+                            result = email
+                            if re.search(regex, result):
+                                users[email] = {
+                                    'name': name,
+                                    'username': username,
+                                    'email': email,
+                                    'pass': password,
+                                }
+                                print(users)
+                                return 1
+                            else:
+                                return 2
+                        else:
+                            return 3
+                    else:
+                        return 4
+                else:
+                    return 5
+            else:
+                return 6
